f32fcc5d847c925453fb5f0031ddb15881827770
Enid516 <1578802364@qq.com>
2017-06-02 17:06:59 +0800

    first commit

---
+++ app/src/main/java/com/wufu/exrobots/BaseResultModel.java
@@ -0,0 +1,19 @@
+package com.wufu.exrobots;
+
+/**
+ * Created by Enid on 2017/6/1.
+ */
+
+public class BaseResultModel {
+    int rc;
+    String operation;
+    String service;
+    String answer;  //object
+    String text;    //input
+    String moreResults;// nullable , aBaseResultModel -->bike
+
+    String webPage; //object,header-url
+    String semantic; //object
+
+
+}
